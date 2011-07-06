aadd7d45876c22e182d942bae8f73412136785aa
Christoph Junghans <junghans@mpip-mainz.mpg.de>
2011-07-06 12:02:31 +0200

    fixed a typo

--- build.sh
+++ build.sh
@@ -565,7 +565,7 @@ for prog in "$@"; do
     rm -f CMakeCache.txt
   fi
   if [[ $do_cmake == "yes" && -f CMakeLists.txt ]]; then
-    [[ -z $(sed -n '/^project(.*)/p' CMakeLists.txt) ]] && die "The current directory does not look like a source main directory (no project line in CMakeLists.txt found)"
+    [[ -z $(sed -n '/^project(.*)/p' CMakeLists.txt) ]] && die "The current directory ($PWD) does not look like a source main directory (no project line in CMakeLists.txt found)"
     cecho BLUE "cmake -DCMAKE_INSTALL_PREFIX="$prefix" $cmake_opts ."
     [[ $cmake != "cmake" ]] && $cmake  -DCMAKE_INSTALL_PREFIX="$prefix" $cmake_opts .
     # we always run normal cmake in case user forgot to generate
