01697823b2526091b0aec52ad114508b6f1c0f26
JoshuaSBrown <joshbro42867@yahoo.com>
2019-02-06 23:22:57 -0700

    Fixed include that was giving compile time error

--- src/csg_boltzmann/tabulatedpotential.h
+++ src/csg_boltzmann/tabulatedpotential.h
@@ -1,5 +1,5 @@
-/* 
- * Copyright 2009-2011 The VOTCA Development Team (http://www.votca.org)
+/*
+ * Copyright 2009-2019 The VOTCA Development Team (http://www.votca.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,43 +16,42 @@
  */
 
 #ifndef _TABULATEDPOTENTIAL_H
-#define	_TABULATEDPOTENTIAL_H
+#define _TABULATEDPOTENTIAL_H
 
-#include <vector>
+#include "analysistool.h"
 #include "bondedstatistics.h"
+#include <vector>
 #include <votca/tools/histogram.h>
 
 using namespace std;
 using namespace votca::tools;
 using namespace votca::csg;
 
-class TabulatedPotential
-    : public AnalysisTool
-{
-    public:
-        TabulatedPotential();
-        ~TabulatedPotential() {};
+class TabulatedPotential : public AnalysisTool {
+ public:
+  TabulatedPotential();
+  ~TabulatedPotential(){};
 
-        void Register(map<string, AnalysisTool *> &lib);
+  void Register(map<string, AnalysisTool *> &lib);
 
-        void Command(BondedStatistics &bs, string cmd, vector<string> &args);
-        void Help(string cmd, vector<string> &args);
+  void Command(BondedStatistics &bs, string cmd, vector<string> &args);
+  void Help(string cmd, vector<string> &args);
 
-        void WriteHistogram(BondedStatistics &bs, vector<string> &args);
-        void WritePotential(BondedStatistics &bs, vector<string> &args);
+  void WriteHistogram(BondedStatistics &bs, vector<string> &args);
+  void WritePotential(BondedStatistics &bs, vector<string> &args);
 
-    private:
-        bool SetOption(Histogram::options_t &op, const vector<string> &args);
-        void Smooth(vector<double> &data, bool bPeriodic);
-        void BoltzmannInvert(vector<double> &data, double T);
-        void CalcForce(vector<double> &u, vector<double> &du, double dx, bool bPeriodic);
+ private:
+  bool SetOption(Histogram::options_t &op, const vector<string> &args);
+  void Smooth(vector<double> &data, bool bPeriodic);
+  void BoltzmannInvert(vector<double> &data, double T);
+  void CalcForce(vector<double> &u, vector<double> &du, double dx,
+                 bool bPeriodic);
 
-        Histogram::options_t _tab_options;
-        Histogram::options_t _hist_options;
-        int _tab_smooth1;
-        int _tab_smooth2;
-        double _T;
+  Histogram::options_t _tab_options;
+  Histogram::options_t _hist_options;
+  int _tab_smooth1;
+  int _tab_smooth2;
+  double _T;
 };
 
-#endif	/* _TABULATEDPOTENTIAL_H */
-
+#endif /* _TABULATEDPOTENTIAL_H */
