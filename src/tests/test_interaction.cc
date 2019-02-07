a537cfbfc5434f39d8d3d6441dd14d1d41320ec5
JoshuaSBrown <joshbro42867@yahoo.com>
2019-02-07 10:58:42 -0700

    Added type correction to BeadCount calls

--- src/tests/test_interaction.cc
+++ src/tests/test_interaction.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2018 The VOTCA Development Team (http://www.votca.org)
+ * Copyright 2009-2019 The VOTCA Development Team (http://www.votca.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,7 +22,6 @@
 
 #include <string>
 #include <votca/csg/bead.h>
-#include <votca/csg/beadtype.h>
 #include <votca/csg/interaction.h>
 #include <votca/csg/molecule.h>
 #include <votca/csg/topology.h>
